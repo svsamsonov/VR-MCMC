8930a69ee4770afbc5d98ca187b48cb8477a4410
sergo_samsonoff <svsamsonov@hse.ru>
2020-02-24 18:00:13 +0300

    code update: multidimensional case

--- Code/martingale.py
+++ Code/martingale.py
@@ -84,12 +84,48 @@ def approx_q(X_train,Y_train,N_traj_train,lag,max_deg):
             coefs_poly = np.concatenate((coefs_poly,coefs),axis=0)
     return coefs_poly
 
-def test_traj(r_seed,max_deg):
+def get_indices_poly(ind,K_max,S_max):
+    """
+    Transforms 1d index into 2d index
+    """
+    S = ind % (S_max + 1)
+    K = ind // (S_max + 1) 
+    return K,S
+
+def init_basis_polynomials(K_max,S_max,st_norm_moments,gamma):
+    """
+    Represents E[H_k(\xi)*(x-\gamma \mu(x) + \sqrt{2\gamma}\xi)^s] as a polynomial of variable $y$, where y = x - \gamma \mu(x)
+    Args:
+        K_max - maximal degree of Hermite polynomial;
+        S_max - maximal degree of regressor polynomial;
+        st_norm_moments - array containing moments of standard normal distribution;
+    Return:
+        Polynomial coefficients
+    """
+    Poly_coefs_regression = np.zeros((K_max+1,S_max+1,S_max+1),dtype = float)
+    for k in range(Poly_coefs_regression.shape[0]):
+        for s in range(Poly_coefs_regression.shape[1]):
+            herm_poly = np.zeros(K_max+1, dtype = float)
+            herm_poly[k] = 1.0
+            herm_k = P.hermite_e.herme2poly(herm_poly)
+            herm_k = herm_k / np.sqrt(sp.special.factorial(k))
+            c = np.zeros(S_max+1, dtype = float)
+            for deg in range(s+1):
+                c[deg] = (np.sqrt(2*gamma)**(s-deg))*sp.special.binom(s,deg)*np.dot(herm_k,st_norm_moments[(s-deg):(s - deg+len(herm_k))])
+            Poly_coefs_regression[k,s,:] = c
+    return Poly_coefs_regression
+
+def compute_a_val(y,coefs,k):
+    """
+    compute values of a scalar product of a given polynomial with  
+    """
+
+def test_traj(r_seed,K_max):
     """
     """
     X_test,Noise = ULA(r_seed,Potential,step, N, n, d, return_noise = True)
     #compute number of basis polynomials
-    num_basis_funcs = (max_deg+1)**d - 1
+    num_basis_funcs = (K_max+1)**d
     print("number of basis functions = ",num_basis_funcs)
     #compute polynomials of noise variables Z_l
     poly_vals = np.zeros((num_basis_funcs,N_test),dtype = float)
@@ -98,40 +134,26 @@ def test_traj(r_seed,max_deg):
     #initialize function
     f_vals_vanilla = set_func(X_test)
     cvfs = np.zeros_like(f_vals_vanilla)
+    table_coefs = init_basis_polynomials(K_max,S_max,st_norm_moments,step)
     for i in range(1,len(cvfs)):
         #start computing a_{p-l} coefficients
         num_lags = min(lag,i)
-        a_vals = np.zeros((num_lags,num_basis_funcs),dtype = float)#cobtrol values
-        
-        for nfunc in range(num_lags):#for a fixed lag Q function
+        a_vals = np.zeros((num_lags,num_basis_funcs),dtype = float)#control variates
+        for func_order in range(num_lags):#for a fixed lag Q function
             #compute \hat{a} with fixed lag
-            x = X_test[i-1-nfunc]#should be -2 here?
-            a_cur = np.zeros(...,dtype=float)
-            for m in range(len(coefs_poly[0])):
-                poly_vspom = np.zeros((max_deg_regr+1)*d,dtype=float)
-                poly_indices = split_index(m,d,max_deg_regr)
-                print(poly_indices)
-                for u in range(m+1):
-                    deg = u % d
-                    poly_vspom[u] = ((x-gamma*Cur_pot.gradpotential(x))**(m-deg))*((np.sqrt(2*gamma))**deg)*sp.special.binom(m,deg)
-                #print(poly_vspom)
-                a_cur = P.polynomial.polyadd(a_cur,coefs_poly[npol,m]*poly_vspom)
-            #print("a_cur")
-            #print(a_cur)
-            for k in range(1,max_deg+1):
-                c = np.zeros(max_deg+1)
-                c[k] = 1
-                herm_coef = P.hermite_e.herme2poly(c)
-                #normalize now
-                herm_coef = herm_coef / np.sqrt(sp.special.factorial(k))
-                integr_coefs = P.polynomial.polymul(herm_coef,a_cur)
-                #Note that a_vals are stored in reversed order
-                a_vals[-(npol+1),k] = np.dot(integr_coefs,moments_stand_norm[:len(integr_coefs)])
+            x = X_test[i-1-nfunc]
+            x_next = x - gamma*Cur_pot.gradpotential(x)
+            for k in range(1,num_basis_funcs+1):
+                a_cur = np.ones(..., dtype = float)
+                for s in range(len(a_cur)):
+                    k_vect,s_vect = get_representation(k,s)
+                    for dim_ind in range(len(k_vect)):
+                        a_cur[s] = a_cur[s]*P.polynomial.polyval(x_next,table_coefs[x_next,k_vect[dim_ind],s_vect[dim_ind],:])
+                a_vals[-(npol+1),k] = np.dot(a_cur,coefs_poly[npol,:])
             #OK, now I have coefficients of the polynomial, and I need to integrate it w.r.t. Gaussian measure
         #print(a_vals.shape)
         #print(a_vals)
         cvfs[i] = np.sum(a_vals*(poly_vals[:,i-num_poly+1:i+1].T))
-        #cvfs[i] = np.sum(np.mean(a_vals,axis = 0))
         if (i%100 == 0):
             print("100 observations proceeded")
         #save results
